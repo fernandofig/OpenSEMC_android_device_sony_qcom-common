bd58db94477266e93e4d211d1ca68a3a6a5da1f4
RaymanFX <raymanfx@gmail.com>
2013-07-17 13:09:44 +0200

    qcom-common: switch common path call

--- qcom-common.mk
+++ qcom-common.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-COMMON_PATH := device/sony/qcom-common
+COMMON_PATH := $(LOCAL_PATH)
 
 DEVICE_PACKAGE_OVERLAYS += $(COMMON_PATH)/overlay
 
